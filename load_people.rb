05c9df47e823125100775dd2e5b0744d6a04f813
Rohit Kumar <rohit.kumar@rutgers.edu>
2013-11-17 18:29:50 -0500

    some work on hipsters

--- load_people.rb
+++ load_people.rb
@@ -2,17 +2,49 @@ require 'rubygems'
 require 'sequel'
 require 'csv'
 
+#Constants / Settings
+odd_beers = ["Redhook Nut Brown", "Redbook IPA", "Redhook Hefe-weizen", "Redhook ESB",
+             "Redhook Blonde Ale", "Stone Imperial Russian Stout", "Serpents Stout",
+             "Old Rasputin Russian Imperial Stout", "Guiness Extra Stout", "Dragon Stout"]
+
 csv_file = 'names.csv'
+total_count = 10_000
+hipster_rate = 0.25
+hipster_count = 250
+current_hipsters = 0
 user = ENV['cs336user'] || "Your user here"
 pass = ENV['cs336pass'] || "Your pass here"
 
+#DB connection
 DB = Sequel.connect("mysql2://#{user}:#{pass}@cs336-24.cs.rutgers.edu/project")
-drinkers = DB[:drinkers]
 
+#Tables
+drinkers = DB[:drinkers]
+likes = DB[:likes]
+favorite = DB[:favorite]
+
+id = 1;
 CSV.foreach(csv_file, :headers => :first_row) do |row|
-  user = {:name => "#{row[0]} #{row[1]}",
-                  :gender => row[2], :address => row[3],
-                  :city => row[4], :phone => row[5],
-                  :age => 2013 - row[6].split('/')[2].to_i}
+  hipster = false
+  user = {:id => id, :name => "#{row[0]} #{row[1]}",
+          :gender => row[2], :address => row[3],
+          :city => row[4], :phone => row[5],
+          :age => 2013 - row[6].split('/')[2].to_i}
   drinkers.insert(user)
+  id += 1;
+
+  #If hipster then they like weird stuff
+  if (user[:age] >= 22 && user[:age] <= 27 && current_hipsters < hipster_count)
+    favorite_beer = odd_beers.sample
+    favorite.insert({ :drinker_id => user[:id], :beer => favorite_beer })
+    likes.insert({ :drinker_id => user[:id], :beer => favorite_beer })
+    new_beers = odd_beers - [favorite_beer]
+    rand(2..5).times do
+      beer = new_beers.sample
+      new_beers = new_beers - [beer]
+      likes.insert({ :drinker_id => user[:id], :beer => beer })
+    end
+  else
+    #TODO add normal favorite and likes
+  end
 end
