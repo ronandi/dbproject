7c85b87621da2087eaa6abb1acf4668f328b64de
Rohit Kumar <rohit.kumar@rutgers.edu>
2013-11-18 01:17:11 -0500

    Populate frequents and sells. Also, populate bars and beers if they are empty

--- load_people.rb
+++ load_people.rb
@@ -11,9 +11,7 @@ firstbeer_array = ['Budweiser Light', 'Budweiser Light Platinum', 'Busch Light',
                    'Coors Light', 'Corona Light', 'Heineken Light', 'Miller Lite',
                    'Yuengling Light']
 
-csv_file = 'names.csv'
-total_count = 10_000
-hipster_rate = 0.25
+csv_file = 'data/names.csv'
 hipster_count = 250
 current_hipsters = 0
 user = ENV['cs336user'] || "Your user here"
@@ -28,17 +26,23 @@ likes = DB[:likes]
 favorite = DB[:favorite]
 firstbeer = DB[:firstbeer]
 beerDB = DB[:beers]
+bars = DB[:bars]
+sells = DB[:sells]
+frequents = DB[:frequents]
+DB.run File.read('queries/beers.sql') if beerDB.count == 0
+DB.run File.read('queries/bars.sql') if bars.count == 0
+bar_names = bars.select(:name).all.map { |bar| bar[:name] }
 
 #Populate database
 id = 1;
 CSV.foreach(csv_file, :headers => :first_row) do |row|
-  hipster = false
   user = {:id => id, :name => "#{row[0]} #{row[1]}",
           :gender => row[2], :address => row[3],
           :city => row[4], :phone => row[5],
           :age => 2013 - row[6].split('/')[2].to_i}
   #drinkers.insert(user)
   id += 1;
+  favorite_beer = nil
 
   #If hipster then they like weird stuff
   if (user[:age] >= 22 && user[:age] <= 27 && current_hipsters < hipster_count)
@@ -54,35 +58,42 @@ CSV.foreach(csv_file, :headers => :first_row) do |row|
       likes.insert({ :drinker_id => user[:id], :beer => beer })
     end
   else
-    entry = {:drinker_id => user[:id], :beer => firstbeer_array.sample,
-             :year => (2013 - (user[:age] - 21))}
+    entry = { :drinker_id => user[:id], :beer => firstbeer_array.sample,
+             :year => (2013 - (user[:age] - 21)) }
     firstbeer.insert(entry)
 
-    fbeer = nil
     if(user[:gender] = 'female')
       if(user[:age] < 30)
         farray = beerDB.select(:name).where{ibu < 25}
-        fbeer = farray.to_a.sample[:name]
+        favorite_beer = farray.to_a.sample[:name]
       elsif (user[:age] < 40)
         farray = beerDB.select(:name).where{ibu < 21}
-        fbeer = farray.to_a.sample[:name]
+        favorite_beer = farray.to_a.sample[:name]
       else
         farray = beerDB.select(:name).where{ibu < 16}
-        fbeer = farray.to_a.sample[:name]
+        favorite_beer = farray.to_a.sample[:name]
       end
     else
       if(user[:age] < 30)
         farray = beerDB.select(:name).where{ibu > 25}
-        fbeer = farray.to_a.sample[:name]
+        favorite_beer = farray.to_a.sample[:name]
       elsif (user[:age] < 40)
         farray = beerDB.select(:name).where{ibu > 34}
-        fbeer = farray.to_a.sample[:name]
+        favorite_beer = farray.to_a.sample[:name]
       else
         farray = beerDB.select(:name).where{ibu > 55}
-        fbeer = farray.to_a.sample[:name]
+        favorite_beer = farray.to_a.sample[:name]
       end
     end
-    entry = {:drinker_id => user[:id], :beer => fbeer}
+    entry = {:drinker_id => user[:id], :beer => favorite_beer}
     favorite.insert(entry)
   end
+
+  #Add frequents
+  random_bar = bar_names.sample
+  frequents.insert({ :drinker_id => user[:id], :bar => random_bar})
+  if sells.where(:bar => random_bar, :beer => favorite_beer).first.nil?
+    sells.insert(:bar => random_bar, :beer => favorite_beer, :price => rand(2..6).round(2))
+  end
+
 end
